c75c504493bb2e03f321f670f23a3af47127bd42
Samuel Vinnik <themansamuel3@gmail.com>
2023-11-17 14:13:36 -0600

    added movies table, created simple list route for 'movies'

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://qazmjvyo:e1RU62mJTfJsw74ugCl8sP6sSTESljGZ@otto.db.elephantsql.com/qazmjvyo",
 } = process.env;
 
 module.exports = {
@@ -19,29 +19,29 @@ module.exports = {
     },
   },
 
-  production: {
-    client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-  },
+  // production: {
+  //   client: "postgresql",
+  //   connection: DATABASE_URL,
+  //   pool: { min: 0, max: 5 },
+  //   migrations: {
+  //     directory: path.join(__dirname, "src", "db", "migrations"),
+  //   },
+  //   seeds: {
+  //     directory: path.join(__dirname, "src", "db", "seeds"),
+  //   },
+  // },
 
-  test: {
-    client: "sqlite3",
-    connection: {
-      filename: ":memory:",
-    },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-    useNullAsDefault: true,
-  },
+  // test: {
+  //   client: "sqlite3",
+  //   connection: {
+  //     filename: ":memory:",
+  //   },
+  //   migrations: {
+  //     directory: path.join(__dirname, "src", "db", "migrations"),
+  //   },
+  //   seeds: {
+  //     directory: path.join(__dirname, "src", "db", "seeds"),
+  //   },
+  //   useNullAsDefault: true,
+  // },
 };
--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const knex = require("./db/connection");
+
+app.use(express.json());
+
+async function getMoviesFromTable() {
+  console.log("HELLO!------");
+  return knex("movies").select("*");
+}
+
+async function list(req, res) {
+  const data = await getMoviesFromTable();
+
+  res.json({ data });
+}
+
+app.get("/movies", list);
 
 module.exports = app;
